b7813a6bb35888a801012c9b17e5c0ca3fbdc343
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2016-04-28 11:18:44 +0900

    動作確認用のテストを追加

---
+++ Tests/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Plugin\Point\Tests;
+
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(true);
+    }
+}
