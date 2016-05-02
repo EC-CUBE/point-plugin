9ac929e61d9da0185250f6191d1c7fa18d1bb743
lammn <mnlam@lockon-vn.com>
2016-05-02 11:04:17 +0900

    add test repository

---
+++ Tests/Repository/PointCustomerRepositoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Eccube\Tests\Repository;
+
+use Eccube\Application;
+use Eccube\Tests\EccubeTestCase;
+
+/**
+ * Class PointInfoRepositoryTest
+ *
+ * @package Eccube\Tests\Repository
+ */
+class PointCustomerRepositoryTest extends EccubeTestCase
+{
+    public function testSavePoint(){
+        $Customer = $this->createCustomer();
+        $PointCustomer = $this->app['eccube.plugin.point.repository.pointcustomer']->savePoint(100, $Customer);
+        $this->expected = 100;
+        $this->actual = $PointCustomer->getPlgPointCurrent();
+        $this->verify();
+    }
+
+    public function testGetLastPointById(){
+        $Customer = $this->createCustomer();
+        $PointCustomer = $this->app['eccube.plugin.point.repository.pointcustomer']->savePoint(101, $Customer);
+        $point = $this->app['eccube.plugin.point.repository.pointcustomer']->getLastPointById($Customer->getId());
+        $this->expected = 101;
+        $this->actual = $point;
+        $this->verify();
+    }
+
+
+
+}
+
