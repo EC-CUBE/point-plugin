951d1f69f3b71953f487c7f1bb938d52e083189b
ryo-endo <to.endo.dev@gmail.com>
2016-05-13 20:21:34 +0900

    リファクタ：エラーが表示の処理を関数化。

--- PointEvent.php
+++ PointEvent.php
@@ -180,30 +180,11 @@ class PointEvent
      */
     public function onRenderShoppingComplete(TemplateEvent $event)
     {
-        // 不適切な受注記録に、今回の受注が含まれているか？
-        $parameters = $event->getParameters();
-        $orderId = $parameters['orderId'];
-        $result = $this->app['eccube.plugin.point.repository.pointabuse']->findBy(array('order_id' => $orderId));
-        if (empty($result)) {
-            return;
+        // ログイン判定
+        if ($this->isAuthRouteFront()) {
+            $helper = new FrontShoppingComplete();
+            $helper->createTwig($event);
         }
-
-        // エラーメッセージの挿入
-        $search = '{% block main %}';
-        $script = <<<__EOL__
-{% block javascript %}
-            <script>
-            $(function() {
-                $("#deliveradd_input_box__message").children("h2.heading01").remove();
-                $("#deliveradd_input_box__message").prepend('<div class="message"><p class="errormsg bg-danger">ご注文中に問題が発生した可能性があります。お手数ですがお問い合わせをお願いします。(受注番号：{{ orderId }})</p></div>');
-            });
-            </script>
-{% endblock %}
-__EOL__;
-
-        $replace = $search.$script;
-        $source = str_replace($search, $replace, $event->getSource());
-        $event->setSource($source);
     }
 
     /**
