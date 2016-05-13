d3777c338493ff1be4147f29609b4f83fb101495
ryo-endo <to.endo.dev@gmail.com>
2016-05-13 14:43:42 +0900

    不適切な受注記録用テーブルの追加。

---
+++ Repository/PointAbuseRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+* This file is part of EC-CUBE
+*
+* Copyright(c) 2000-2016 LOCKON CO.,LTD. All Rights Reserved.
+* http://www.lockon.co.jp/
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+namespace Plugin\Point\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class PointAbuseRepository
+ * @package Plugin\Point\Repository
+ */
+class PointAbuseRepository extends EntityRepository
+{
+}
