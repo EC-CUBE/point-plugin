3a8ca2e6ef6c9b6d3328590f85f0f87f52c364ab
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-05-17 15:55:57 +0900

    PointInfoType のテストケース追加

---
+++ Tests/Form/Type/PointInfoTypeTest.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+namespace Eccube\Tests\Form\Type\Admin;
+
+class PointInfoTypeTest extends \Eccube\Tests\Form\Type\AbstractTypeTestCase
+{
+    /** @var \Eccube\Application */
+    protected $app;
+
+    /** @var \Symfony\Component\Form\FormInterface */
+    protected $form;
+
+    /** @var array デフォルト値（正常系）を設定 */
+    protected $formData = array(
+        'plg_add_point_status' => 1,
+        'plg_calculation_type' => \Plugin\Point\Entity\PointInfo::POINT_CALCULATE_SUBTRACTION,
+        'plg_basic_point_rate'=> 100,
+        'plg_point_conversion_rate' => 100,
+        'plg_round_type' => \Plugin\Point\Entity\PointInfo::POINT_ROUND_FLOOR
+    );
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // CSRF tokenを無効にしてFormを作成
+        $this->form = $this->app['form.factory']
+            ->createBuilder('admin_point_info', null, array(
+                'csrf_protection' => false,
+            ))
+            ->getForm();
+    }
+
+    public function testValidData()
+    {
+        $this->form->submit($this->formData);
+        $this->assertTrue($this->form->isValid(), $this->form->getErrorsAsString());
+    }
+}
